da6833a2fe3b065360101ced928806508a1f3366
Rachel <rachelkane04@gmail.com>
2023-10-26 12:29:04 +0100

    temp changes for 20 grid Changes to be committed: 	new file:   np_script.sh 	new file:   script1.sh 	new file:   test_results/10nodes_10steps_20grid/T_0.0/LL-Output-Thu-26-Oct-2023-at-11-43-08AM-20-0.0.txt 	new file:   test_results/10nodes_10steps_20grid/T_0.0/final_20_0.0.png 	new file:   test_results/10nodes_10steps_20grid/T_0.0/initial_20_0.0.png 	new file:   test_results/10nodes_10steps_20grid/T_0.2/LL-Output-Thu-26-Oct-2023-at-11-43-08AM-20-0.2.txt 	new file:   test_results/10nodes_10steps_20grid/T_0.2/final_20_0.2.png 	new file:   test_results/10nodes_10steps_20grid/T_0.2/initial_20_0.2.png 	new file:   test_results/10nodes_10steps_20grid/T_0.4/LL-Output-Thu-26-Oct-2023-at-11-43-08AM-20-0.4.txt 	new file:   test_results/10nodes_10steps_20grid/T_0.4/final_20_0.4.png 	new file:   test_results/10nodes_10steps_20grid/T_0.4/initial_20_0.4.png 	new file:   test_results/10nodes_10steps_20grid/T_0.6/LL-Output-Thu-26-Oct-2023-at-11-43-18AM-20-0.6.txt 	new file:   test_results/10nodes_10steps_20grid/T_0.6/final_20_0.6.png 	new file:   test_results/10nodes_10steps_20grid/T_0.6/initial_20_0.6.png 	new file:   test_results/10nodes_10steps_20grid/T_0.8/LL-Output-Thu-26-Oct-2023-at-11-53-10AM-20-0.8.txt 	new file:   test_results/10nodes_10steps_20grid/T_0.8/final_20_0.8.png 	new file:   test_results/10nodes_10steps_20grid/T_0.8/initial_20_0.8.png 	renamed:    test_results/10nodes_10steps_20grid/LL-Output-Wed-25-Oct-2023-at-03-55-20PM.txt -> test_results/10nodes_10steps_20grid/T_1.0/LL-Output-Wed-25-Oct-2023-at-03-55-20PM.txt 	renamed:    test_results/10nodes_10steps_20grid/final_20.png -> test_results/10nodes_10steps_20grid/T_1.0/final_20.png 	renamed:    test_results/10nodes_10steps_20grid/initial_20.png -> test_results/10nodes_10steps_20grid/T_1.0/initial_20.png 	new file:   test_results/10nodes_10steps_20grid/T_1.2/LL-Output-Thu-26-Oct-2023-at-11-53-10AM-20-1.2.txt 	new file:   test_results/10nodes_10steps_20grid/T_1.2/final_20_1.2.png 	new file:   test_results/10nodes_10steps_20grid/T_1.2/initial_20_1.2.png 	new file:   test_results/10nodes_10steps_20grid/T_1.4/LL-Output-Thu-26-Oct-2023-at-11-53-10AM-20-1.4.txt 	new file:   test_results/10nodes_10steps_20grid/T_1.4/final_20_1.4.png 	new file:   test_results/10nodes_10steps_20grid/T_1.4/initial_20_1.4.png 	new file:   test_results/10nodes_10steps_20grid/T_1.6/LL-Output-Thu-26-Oct-2023-at-11-53-20AM-20-1.6.txt 	new file:   test_results/10nodes_10steps_20grid/T_1.6/final_20_1.6.png 	new file:   test_results/10nodes_10steps_20grid/T_1.6/initial_20_1.6.png 	new file:   test_results/10nodes_10steps_20grid/T_1.8/LL-Output-Thu-26-Oct-2023-at-12-05-18PM-20-1.8.txt 	new file:   test_results/10nodes_10steps_20grid/T_1.8/final_20_1.8.png 	new file:   test_results/10nodes_10steps_20grid/T_1.8/initial_20_1.8.png 	new file:   test_results/10nodes_10steps_20grid/T_2.0/LL-Output-Thu-26-Oct-2023-at-12-05-18PM-20-2.0.txt 	new file:   test_results/10nodes_10steps_20grid/T_2.0/final_20_2.0.png 	new file:   test_results/10nodes_10steps_20grid/T_2.0/initial_20_2.0.png

---
+++ script1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --job-name=LL_crystal
+#SBATCH --partition=teach_cpu
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=28
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:0:50
+#SBATCH --mem-per-cpu=100M
+#SBATCH --account=phys026162
+
+## Direct output to the following files.
+## (The %j is replaced by the job id.)
+#SBATCH -e _%j.txt
+#SBATCH -o out_%j.txt
+
+# Just in case this is not loaded already...
+module load languages/intel/2020-u4
+module add languages/anaconda3/2020-3.8.5
+
+# Change to working directory, where the job was submitted from.
+cd "${SLURM_SUBMIT_DIR}"
+
+# Record some potentially useful details about the job: 
+echo "Running on host $(hostname)"
+echo "Started on $(date)"
+echo "Directory is $(pwd)"
+echo "Slurm job ID is ${SLURM_JOBID}"
+echo "This jobs runs on the following machines:"
+echo "${SLURM_JOB_NODELIST}" 
+printf "\n\n"
+
+# Submit
+mpiexec -n 10 python LebwohlLasher.py 10 20 2.0 1
+
+# Output the end time
+printf "\n\n"
+echo "Ended on: $(date)"
