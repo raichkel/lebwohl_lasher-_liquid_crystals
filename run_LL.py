8251aea08c4c23edebec7e209966809f7f134d1d
Rachel <rachelkane04@gmail.com>
2023-10-30 14:19:43 +0000

    cythoned code w/out edits Changes to be committed: 	new file:   LL_runs/LL-Output-Mon-30-Oct-2023-at-02-17-35PM.txt 	new file:   LL_runs/final_20.png 	new file:   LL_runs/initial_20.png 	new file:   LebwohlLasher.c 	new file:   LebwohlLasher.cp311-win_amd64.pyd 	new file:   build/lib.win-amd64-cpython-311/LebwohlLasher.cp311-win_amd64.pyd 	new file:   build/temp.win-amd64-cpython-311/Release/LebwohlLasher.cp311-win_amd64.exp 	new file:   build/temp.win-amd64-cpython-311/Release/LebwohlLasher.cp311-win_amd64.lib 	new file:   build/temp.win-amd64-cpython-311/Release/LebwohlLasher.obj 	modified:   run_LL.py

--- run_LL.py
+++ run_LL.py
@@ -1,8 +1,18 @@
 import sys
 from LebwohlLasher import main
 
-if int(len(sys.argv)) == 2:
-    main(int(sys.argv[1]))
-else:
-    print("Usage: {} <ITERATIONS>".format(sys.argv[0]))
+# if int(len(sys.argv)) == 2:
+#     main(int(sys.argv[1]))
+# else:
+#     print("Usage: {} <ITERATIONS>".format(sys.argv[0]))
 
+
+if int(len(sys.argv)) == 5:
+        PROGNAME = sys.argv[0]
+        ITERATIONS = int(sys.argv[1])
+        SIZE = int(sys.argv[2])
+        TEMPERATURE = float(sys.argv[3])
+        PLOTFLAG = int(sys.argv[4])
+        main(PROGNAME, ITERATIONS, SIZE, TEMPERATURE, PLOTFLAG)
+else:
+    print("Usage: python {} <ITERATIONS> <SIZE> <TEMPERATURE> <PLOTFLAG>".format(sys.argv[0]))
